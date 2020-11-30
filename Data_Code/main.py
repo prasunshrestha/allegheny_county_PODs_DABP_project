0fbdc9f4a1d8f17d15e3c776a78956c470e6d026
ruthwang23 <33666422+ruthwang23@users.noreply.github.com>
2020-11-30 11:36:43 -0500

    Add files via upload

--- Data_Code/main.py
+++ Data_Code/main.py
@@ -11,8 +11,19 @@ course = "94-867 Decision Analytics for Business & Policy Fall 2020"
 team = "Florian Cords, Jose Salomon, Prasun Shrestha, Ruth Wang"
 print('\n', title, course, team, sep='\n')
 
-welcome = ".."
-instructions = "..."
+welcome = "\"welcome message\""
+print("\nWould you like to update the distances")
+update = input("Enter Y or N: ")
+if update.upper() == "N":
+	status = False
+elif update.upper() == "Y":
+	print("Distances Updated")
+	status = False
+else:
+	print("Invalid selection. Please try again.")
+	cont = input("Enter Y or N: ")
+
+instructions = "\"instructions\""
 print('\n', welcome, instructions, sep='\n')
 
 choice = input("Enter your value: ")
